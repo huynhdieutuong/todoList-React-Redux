47a0a9b2dfb19b0f65e869ee1a337fda9a96576f
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-05-06 17:01:22 +0700

    Updates

---
+++ precache-manifest.996cb940a92224bed8015304ab340965.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "93998245429b2bddf3885d142fa6a0dd",
+    "url": "/todoList-React-Redux/static/media/delete.93998245.svg"
+  },
+  {
+    "revision": "e66fc63d86c1d4b4a636264be56664b9",
+    "url": "/todoList-React-Redux/static/media/checked.e66fc63d.svg"
+  },
+  {
+    "revision": "3a424c990b7aeacbb6e6",
+    "url": "/todoList-React-Redux/static/js/runtime~main.51dd22c7.js"
+  },
+  {
+    "revision": "fd57aea24b61d2b60d35",
+    "url": "/todoList-React-Redux/static/js/main.7c6830db.chunk.js"
+  },
+  {
+    "revision": "131ecb8a299f55c1983d",
+    "url": "/todoList-React-Redux/static/js/2.6a7440e2.chunk.js"
+  },
+  {
+    "revision": "fd57aea24b61d2b60d35",
+    "url": "/todoList-React-Redux/static/css/main.8248615d.chunk.css"
+  },
+  {
+    "revision": "131ecb8a299f55c1983d",
+    "url": "/todoList-React-Redux/static/css/2.9f770f15.chunk.css"
+  },
+  {
+    "revision": "23ebe8a2c84344603d70295d37b1e3d2",
+    "url": "/todoList-React-Redux/index.html"
+  }
+];
